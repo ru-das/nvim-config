0cf163fce05a49442eeed98894a855f529648c36
Rupam Das <73683964+ru-das@users.noreply.github.com>
2025-09-03 15:16:10 +0530

    override colors for catppuccin colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,8 +5,17 @@ return {
 	priority = 1000,
 	opts = {
 		flavour = "mocha",
+		auto_integrations = true,
+		color_overrides = {
+			mocha = {
+				base = "#11111b",
+				crust = "#1e1e2e",
+			}
+		}
+		
 	},
-	config = function()
-		vim.cmd.colorscheme "catppuccin"
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
