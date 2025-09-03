31d7f428a8eaaecdce1d9a850bf39944c9413ccd
Rupam Das <73683964+ru-das@users.noreply.github.com>
2025-09-03 10:29:41 +0530

    fix nvim-treesitter not installing "ensure_installed"

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,31 +3,34 @@ return {
 	branch = "master",
 	lazy = false,
 	build = ":TSUpdate",
-	opts = {
-		auto_install = true,
-
-		ensure_installed = {
-			"c",
-			"cpp",
-			"css",
-			"diff",
-			"git_config",
-			"git_rebase",
-			"gitattributes",
-			"gitcommit",
-			"gitignore",
-			"html",
-			"javascript",
-			"json",
-			"lua",
-			"markdown",
-			"markdown_inline",
-			"powershell",
-			"python",
-			"query",
-			"rust",
-			"vim",
-			"vimdoc",
+	config = function()
+		require("nvim-treesitter.configs").setup {
+			auto_install = true,
+			prefer_git = false, -- for Windows
+			compilers = { "gcc" }, -- for Windows
+			ensure_installed = {
+				"c",
+				"cpp",
+				"css",
+				"diff",
+				"git_config",
+				"git_rebase",
+				"gitattributes",
+				"gitcommit",
+				"gitignore",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"powershell",
+				"python",
+				"query",
+				"rust",
+				"vim",
+				"vimdoc",
+			}
 		}
-	}
+	end,
 }
